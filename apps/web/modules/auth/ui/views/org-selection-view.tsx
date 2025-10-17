15ed2019f5eb6539d59b506f438099328c79569f
Shaheer Raja <rajashaheer512@gmail.com>
2025-10-17 17:21:45 +0500

    organizations

---
+++ apps/web/modules/auth/ui/views/org-selection-view.tsx
@@ -0,0 +1,15 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export const OrgSelectionView = () =>{
+    return (
+        <div>
+            <OrganizationList
+            afterCreateOrganizationUrl="/"
+            afterSelectOrganizationUrl="/"
+            hidePersonal
+            skipInvitationScreen
+            />
+            
+        </div>
+    )
+}
