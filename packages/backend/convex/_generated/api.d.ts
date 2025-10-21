d665d3490f231a0a41ca547cdbe7c9c60233f1b4
Shaheer Raja <rajashaheer512@gmail.com>
2025-10-21 11:52:05 +0500

    widget session

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as public_contactSession from "../public/contactSession.js";
 import type * as users from "../users.js";
 
 import type {
@@ -25,6 +26,7 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  "public/contactSession": typeof public_contactSession;
   users: typeof users;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
